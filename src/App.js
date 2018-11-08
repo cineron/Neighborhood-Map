28c708336cee03e1fa85d6a99a9317432e8d593f
cineron <cineron+gh@gmail.com>
2018-11-07 19:59:49 -0600

    Coding with follow along. Just before adding Axios

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
     this.renderMap()
   }
 
+  // loads the new script (loadGMapScript)
   renderMap = () => {
     loadGMapsScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyB6fUiE5X9kPlFVUyccN9PgVP-gRR-er0c&callback=initMap")
 
@@ -16,6 +17,7 @@ class App extends Component {
     window.initMap = this.initMap;
   }
 
+  //from https://developers.google.com/maps/documentation/javascript/tutorial
   initMap = () => {
     const map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: -34.397, lng: 150.644},
@@ -40,12 +42,19 @@ Follow along from Elharony Youtube video
 https://www.youtube.com/watch?v=W5LhLZqj76s
 */
 
+//"function" keyword can be used b/c this block is outside React. 
+// re-create the copied function from Google Documents to here.
 function loadGMapsScript(url) {
+  //get the "script" tag
   let index = window.document.getElementsByTagName("script")[0];
+
+  //add a <script> tag
   let script = window.document.createElement("script");
   script.src = url;
   script.async = true;
   script.defer = true;
+  
+  //insert this script before other scripts
   index.parentNode.insertBefore(script, index);
 }
 
